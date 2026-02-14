afb477efe5d8f4eb3648ff919e83a2f68aef95ba
DevPunnana <punnanadevendra1812@gmail.com>
2026-02-14 17:09:08 +0530

    Fix formatting and import statement in App.java

--- src/main/java/com/devendra/demo/maven_demo/App.java
+++ src/main/java/com/devendra/demo/maven_demo/App.java
@@ -3,7 +3,7 @@ package com.devendra.demo.maven_demo;
 /**
  * Hello world!
  */
-import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.StringUtils;
 
 public class App {
     public static void main(String[] args) {
@@ -12,5 +12,6 @@ public class App {
         System.out.println(StringUtils.capitalize(name));
     }
 }
+
 
 
