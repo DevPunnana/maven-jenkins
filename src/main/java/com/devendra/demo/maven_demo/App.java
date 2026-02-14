d09cf0b491316da6d7449742a9a6931710ee00d1
DevPunnana <punnanadevendra1812@gmail.com>
2026-02-14 16:42:11 +0530

    Add files via upload

---
+++ src/main/java/com/devendra/demo/maven_demo/App.java
@@ -0,0 +1,15 @@
+package com.devendra.demo.maven_demo;
+
+/**
+ * Hello world!
+ */
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+
+        String name = "devendra";
+        System.out.println(StringUtils.capitalize(name));
+    }
+}
+
