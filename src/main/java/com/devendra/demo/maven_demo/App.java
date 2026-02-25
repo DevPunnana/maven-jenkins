9dadcaf464f2fc9386f64066b656b7e6e9722a02
DevPunnana <punnanadevendra1812@gmail.com>
2026-02-25 15:06:42 +0530

    Fix formatting and clean up App.java

--- src/main/java/com/devendra/demo/maven_demo/App.java
+++ src/main/java/com/devendra/demo/maven_demo/App.java
@@ -8,10 +8,11 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
     public static void main(String[] args) {
 
-        String name = "devendra"
+        String name = "devendra";
         System.out.println(StringUtils.capitalize(name));
     }
 }
+
 
 
 
